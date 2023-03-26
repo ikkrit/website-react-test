6bc12f4b3105ccd8a74a931b2289a3c1000081e1
ikkrit <johan.car@protonmail.com>
2023-03-26 15:30:19 +0200

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css';
 import { Home } from './screens/Home';
 import { Ressources } from './screens/Ressources';
+import { Routes, Route, NavLink} from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <Home/>
+      <nav>
+        <NavLink to='/'>Accueil</NavLink>
+        <NavLink to='/ressources'>Ressources</NavLink>
+      </nav>
+      <Routes>
+        <Route path='/' element={ <Home/> }/>
+        <Route path='/ressources' element={ <Ressources/>}/>
+      </Routes>
     </>
   );
 }
